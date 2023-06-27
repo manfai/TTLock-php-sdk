461cdffea49b8b2bd999576de026cd33e2afa3b4
MWF's Macbook 14 <manfai106@gmail.com>
2023-06-27 13:30:19 +0800

    hotfix

--- src/ICCard.php
+++ src/ICCard.php
@@ -56,6 +56,63 @@ class ICCard extends TTLockAbstract
 
 	/**
 	 * @param int $lockId
+	 * @param int $pageNo
+	 * @param int $pageSize
+	 * @param int $date
+	 * @return array
+	 * @throws \GuzzleHttp\Exception\GuzzleException | \Exception
+	 * @author manfai
+	 */
+	public function add( int $lockId, int $pageNo = 1, int $pageSize = 20) : array
+	{
+		$response = $this->client->request( 'POST', '/v3/identityCard/list', [
+			'form_params' => [
+				'clientId'    => $this->clientId,
+				'accessToken' => $this->accessToken,
+				'pageNo'      => $pageNo,
+				'pageSize'    => $pageSize,
+				'date'        => number_format(round(microtime(true) * 1000),0,'.','')
+			],
+		] );
+		$body     = json_decode( $response->getBody()->getContents(), true );
+		if( $response->getStatusCode() === 200 && !isset( $body['errcode'] ) ){
+			return (array)$body;
+		} else{
+			throw new \Exception( "errcode {$body['errcode']} errmsg {$body['errmsg']} errmsg : {$body['errmsg']}" );
+		}
+	}
+
+		/**
+	 * @param int $lockId
+	 * @param int $pageNo
+	 * @param int $pageSize
+	 * @param int $date
+	 * @return array
+	 * @throws \GuzzleHttp\Exception\GuzzleException | \Exception
+	 * @author manfai
+	 */
+	public function delete( int $lockId, int $pageNo = 1, int $pageSize = 20) : array
+	{
+		$response = $this->client->request( 'POST', '/v3/identityCard/list', [
+			'form_params' => [
+				'clientId'    => $this->clientId,
+				'accessToken' => $this->accessToken,
+				'pageNo'      => $pageNo,
+				'pageSize'    => $pageSize,
+				'date'        => number_format(round(microtime(true) * 1000),0,'.','')
+			],
+		] );
+		$body     = json_decode( $response->getBody()->getContents(), true );
+		if( $response->getStatusCode() === 200 && !isset( $body['errcode'] ) ){
+			return (array)$body;
+		} else{
+			throw new \Exception( "errcode {$body['errcode']} errmsg {$body['errmsg']} errmsg : {$body['errmsg']}" );
+		}
+	}
+
+
+	/**
+	 * @param int $lockId
 	 * @param int $cardId
 	 * @param int $startDate
 	 * @param int $endDate
@@ -74,6 +131,39 @@ class ICCard extends TTLockAbstract
 				'startDate'   => $startDate,
 				'endDate'     => $endDate,
 				'changeType'  => 2,
+				'date'        => number_format(round(microtime(true) * 1000),0,'.','')
+			],
+		] );
+		$body     = json_decode( $response->getBody()->getContents(), true );
+		if( $response->getStatusCode() === 200 && !isset( $body['errcode'] ) ){
+			return (array)$body;
+		} else{
+			throw new \Exception( "errcode {$body['errcode']} errmsg {$body['errmsg']} errmsg : {$body['errmsg']}" );
+		}
+	}
+
+	/**
+	 * @param int    $lockId
+	 * @param int    $cardId
+	 * @param string $cardName
+	 * @param int    $startDate
+	 * @param int    $endDate
+	 * @return array
+	 * @throws \GuzzleHttp\Exception\GuzzleException | \Exception
+	 * @author manfai
+	 */
+	public function addICCard( int $lockId, int $cardId, string $cardName, int $startDate, int $endDate) : array
+	{
+		$response = $this->client->request( 'POST', '/v3/identityCard/addForReversedCardNumber', [
+			'form_params' => [
+				'clientId'    => $this->clientId,
+				'accessToken' => $this->accessToken,
+				'lockId'      => $lockId,
+				'cardNumber'  => $cardId,
+				'cardName'	  => $cardName,
+				'startDate'   => $startDate,
+				'endDate'     => $endDate,
+				'addType' 	  => 2,
 				'date'        => number_format(round(microtime(true) * 1000),0,'.','')
 			],
 		] );
