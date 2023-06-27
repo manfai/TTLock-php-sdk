ac2de719ba5e1151b15e53acdf8b8feeedf1f0cc
MWF's Macbook 14 <manfai106@gmail.com>
2023-06-27 15:30:19 +0800

    hotfix

--- src/ICCard.php
+++ src/ICCard.php
@@ -54,34 +54,6 @@ class ICCard extends TTLockAbstract
 		}
 	}
 
-	/**
-	 * @param int $lockId
-	 * @param int $pageNo
-	 * @param int $pageSize
-	 * @param int $date
-	 * @return array
-	 * @throws \GuzzleHttp\Exception\GuzzleException | \Exception
-	 * @author manfai
-	 */
-	public function add( int $lockId, int $pageNo = 1, int $pageSize = 20) : array
-	{
-		$response = $this->client->request( 'POST', '/v3/identityCard/list', [
-			'form_params' => [
-				'clientId'    => $this->clientId,
-				'accessToken' => $this->accessToken,
-				'pageNo'      => $pageNo,
-				'pageSize'    => $pageSize,
-				'date'        => number_format(round(microtime(true) * 1000),0,'.','')
-			],
-		] );
-		$body     = json_decode( $response->getBody()->getContents(), true );
-		if( $response->getStatusCode() === 200 && !isset( $body['errcode'] ) ){
-			return (array)$body;
-		} else{
-			throw new \Exception( "errcode {$body['errcode']} errmsg {$body['errmsg']} errmsg : {$body['errmsg']}" );
-		}
-	}
-
 		/**
 	 * @param int $lockId
 	 * @param int $pageNo
