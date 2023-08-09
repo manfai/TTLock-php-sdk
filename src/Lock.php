a0499cc5ade385698ca3a4e589143f02bb8cde89
MWF's Macbook 14 <manfai106@gmail.com>
2023-08-09 16:53:50 +0800

    hotfix

--- src/Lock.php
+++ src/Lock.php
@@ -194,7 +194,7 @@ class Lock extends TTLockAbstract
 	 * @throws \GuzzleHttp\Exception\GuzzleException | \Exception
 	 * @author 韩文博
 	 */
-	public function detail( int $lockId ) : bool
+	public function detail( int $lockId ) : array
 	{
 		$response = $this->client->request( 'POST', '/v3/lock/detail', [
 			'form_params' => [
@@ -204,9 +204,10 @@ class Lock extends TTLockAbstract
 				'date'        => number_format(round(microtime(true) * 1000),0,'.','')
 			],
 		] );
-		$body     = json_decode( $response->getBody()->getContents(), true );
-		if( $response->getStatusCode() === 200 && isset( $body['errcode'] ) && $body['errcode'] === 0 ){
-			return true;
+		// dd($lockId);
+		$body = json_decode( $response->getBody()->getContents(), true );
+		if( $response->getStatusCode() === 200){
+			return $body;
 		} else{
 			throw new \Exception( "errcode {$body['errcode']} errmsg {$body['errmsg']} errmsg : {$body['errmsg']}" );
 		}
